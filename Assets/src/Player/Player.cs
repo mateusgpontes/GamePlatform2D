48ce229925703ab29d53d61dc887036ee2865cc7
mateusgpontes <mateus.gpontes@hotmail.com>
2020-12-14 00:25:34 -0300

    feat: Create basic interface and logic
    platforms and a player were created, and the logic of acceleration,
    jumping and collision was made

---
+++ Assets/src/Player/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D rig;
+    private bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new  Vector2(Speed, rig.velocity.y);
+
+        if (Input.GetKeyDown("space") && !isJumping)
+        {
+            rig.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.layer == 8){
+            isJumping = false;
+        }
+    }
+}
